e116ea98929af81cb87fe5b03600f8ad3ea64eb2
Joana Campos <joanacamposbarros26@gmail.com>
2025-11-11 11:42:40 -0300

    2025-11-11
    Bug do NaN-NaN-NaN

---
+++ src/main/resources/static/js/firebaseConfig.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB32Kl4TmAlxla77KtZZLHGApEa6Sfs9qw",
+  authDomain: "springpad-7fe41.firebaseapp.com",
+  projectId: "springpad-7fe41",
+  storageBucket: "springpad-7fe41.firebasestorage.app",
+  messagingSenderId: "174807560529",
+  appId: "1:174807560529:web:7c22a8c76bbeabb9aae370"
+};
